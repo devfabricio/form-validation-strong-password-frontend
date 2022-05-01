5f9405760400d5d352b4ffd1fea373eb18e23fc5
Fabrício Augusto <fabricio.mata@gmail.com>
2022-05-01 18:43:42 -0300

    finish form-validation

--- assets/js/form-validation.js
+++ assets/js/form-validation.js
@@ -2,8 +2,16 @@
 
 (() => {
     const form = document.querySelector('[data-form]')
+    const progressBar = document.querySelector('[data-requirement-progressbar]')
     const fields = {}
+    const requirements = {}
+    const state = { passwordStrength: 0 }
     
+    const styleProgressBar = () => {
+        progressBar.style.width = `${state.passwordStrength}%`
+        progressBar.dataset.percentage = state.passwordStrength
+    }
+
     const showMessageError = (field, message) => {
         const { element, errorElement } = field
         element.classList.add('error')
@@ -16,6 +24,15 @@
         element.classList.remove('error')
         errorElement.style.display = 'none'
         errorElement.textContent = ''
+    }
+
+    const checkPasswordStregth = (requirementName, condition) => {
+        if(condition) {
+            state.passwordStrength += 25
+            requirements[requirementName].classList.add('checked')
+        } else {
+            requirements[requirementName].classList.remove('checked')
+        }
     }
 
     const validateRequiredFields = () => {
@@ -31,6 +48,37 @@
         return isInvalid
     }
 
+    const validatePasswordStrength = () => {
+        let isInvalid = false
+        const field = fields['password']
+        if(state.passwordStrength < 100) {
+            isInvalid = true
+            showMessageError(field, 'Digte uma senha válida!')
+        }
+        return isInvalid
+    }
+
+    const validateEmail = () => {
+        let isInvalid = false
+        const field = fields['email']
+        const { value } = field.element
+        if(!value.match(/^[\w\.]+@\w+(\.\w+)+$/)) {
+            isInvalid = true
+            showMessageError(field, 'Digte um e-mail válido!')
+        }
+        return isInvalid
+    }
+
+    const onInputPasswordKeyup = (event) => {
+        const { value } = event.target
+        state.passwordStrength = 0
+        checkPasswordStregth('lowerUpperCase', value.match(/[a-z]/) && value.match(/[A-Z]/))
+        checkPasswordStregth('number', value.match(/[0-9]/))
+        checkPasswordStregth('specialCharacter', value.match(/[!@#$%\^&*~)\[\]{}?\.(+=\._-]/))
+        checkPasswordStregth('minCharacter', value.length >= 8)
+        styleProgressBar()
+    }
+
     const onInputFocus = (event) => {
         hideMessageError(fields[event.target.name])
     }
@@ -38,6 +86,8 @@
     const onFormSubmit = (event) => {
         event.preventDefault()
         if(validateRequiredFields()) return
+        if(validateEmail()) return
+        if(validatePasswordStrength()) return
         alert('Dados prontos para serem enviados!')
     }
 
@@ -46,6 +96,15 @@
         for (const fieldKey in fields) {
             const { element } = fields[fieldKey]
             element.addEventListener('focus', onInputFocus)
+            if(fieldKey === 'password') element.addEventListener('keyup', onInputPasswordKeyup)
+        }
+    }
+
+    const setRequirementItemsElements = () => {
+        const requirementItemsElements = document.querySelectorAll('[data-requirement-item]')
+        for (const requirementItem of requirementItemsElements) {
+            const requirementName = requirementItem.dataset['requirementItem']
+            requirements[requirementName] = requirementItem
         }
     }
 
@@ -64,6 +123,7 @@
 
     const init = () => {
         setFieldElements()
+        setRequirementItemsElements()
         setListeners()
     }
 
